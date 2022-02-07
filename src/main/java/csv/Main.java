f8aaf3af6e82c7b6a70a36a5a2f8b499629f11f3
Ahmet_Toy <ahmet.toy@tu-darmstadt.de>
2022-02-07 19:59:27 +0100

    CSV-Format auf Excel umgestellt

--- src/main/java/csv/Main.java
+++ src/main/java/csv/Main.java
@@ -9,27 +9,30 @@ import java.nio.file.Paths;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.core.env.SystemEnvironmentPropertySource;
 
 public class Main {
 
   public static void main(String[] args){
     try (
-        Reader reader = Files.newBufferedReader(Paths.get("src/main/resources/file.csv"));
-        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+        Reader reader = Files.newBufferedReader(Paths.get("C:\\Users\\toy\\IdeaProjects\\Resources"));
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.EXCEL);
     ) {
       for (CSVRecord csvRecord : csvParser) {
         // Accessing Values by Column Index
-        String name = csvRecord.get(0);
-        String email = csvRecord.get(1);
+        String vorname = csvRecord.get(0);
+        String name = csvRecord.get(1);
         String phone = csvRecord.get(2);
         String country = csvRecord.get(3);
+        String age = csvRecord.get(4);
 
         System.out.println("Record No - " + csvRecord.getRecordNumber());
         System.out.println("---------------");
+        System.out.println("Vorname : " + vorname);
         System.out.println("Name : " + name);
-        System.out.println("Email : " + email);
         System.out.println("Phone : " + phone);
         System.out.println("Country : " + country);
+        System.out.println("Alter: " + age);
         System.out.println("---------------\n\n");
       }
     } catch (IOException e) {
